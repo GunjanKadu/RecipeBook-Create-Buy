9141850128910a2bfdc53699f9043e9e84319984
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-01-31 20:04:50 +0100

    addedNavigationUsingEventemmiter and ngIf

--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,7 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  recipeSelected: boolean;
+  @Output() isrecipeSelected = new EventEmitter<boolean>();
+
+  shoppingListSelected: boolean;
+  @Output() isshoppingListSelected = new EventEmitter<boolean>();
+
+  constructor() {}
+  onRecipeClick() {
+    this.recipeSelected = true;
+    this.shoppingListSelected = false;
+    this.isrecipeSelected.emit(this.recipeSelected);
+    this.isshoppingListSelected.emit(this.shoppingListSelected);
+  }
+  onShoppingClick() {
+    this.recipeSelected = false;
+    this.shoppingListSelected = true;
+    this.isshoppingListSelected.emit(this.shoppingListSelected);
+    this.isrecipeSelected.emit(this.recipeSelected);
+  }
+}
