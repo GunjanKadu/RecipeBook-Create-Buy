3e0220380bd4dbcd7320875d7660c82fe9a4ed55
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-01-31 20:04:50 +0100

    addedNavigationUsingEventemmiter and ngIf

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  isRecipeSelected: boolean = true;
+  isshoppingListSelected: boolean;
+
+  recipeSelected(event: boolean) {
+    this.isRecipeSelected = event;
+  }
+  shoppingListSelected(event: boolean) {
+    this.isshoppingListSelected = event;
+  }
 }
