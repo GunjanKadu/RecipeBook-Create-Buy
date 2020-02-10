2fa27a47ffad17b553ca2d1a3d3c83042ae90c48
Gunjan Kadu <agunjan.kadu@gmail.com>
2020-02-10 11:05:18 +0100

    Adding ingredients to recipe

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -1,20 +1,23 @@
 import { EventEmitter } from "@angular/core";
 
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredients.model";
 
 export class RecipeService {
   selectedRecipe = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
+      "Tasty Schnitzel",
       "Simply a test",
-      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/healthy-dinner-ideas-tofu-bowl-1574613204.jpg?crop=0.811xw:0.541xh;0.0737xw,0.247xh&resize=640:*"
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/healthy-dinner-ideas-tofu-bowl-1574613204.jpg?crop=0.811xw:0.541xh;0.0737xw,0.247xh&resize=640:*",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 1)]
     ),
     new Recipe(
-      "A Test Recipe 1",
+      "Big Fat Burger",
       "Simply a test 1 ",
-      "https://www.diabetes.org/sites/default/files/styles/crop_large/public/2019-06/Healthy%20Food%20Made%20Easy%20-min.jpg"
+      "https://www.diabetes.org/sites/default/files/styles/crop_large/public/2019-06/Healthy%20Food%20Made%20Easy%20-min.jpg",
+      [new Ingredient("Wurst", 1), new Ingredient("Cheese Breads", 1)]
     )
   ];
 
