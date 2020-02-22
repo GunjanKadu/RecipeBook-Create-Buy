7919a5173e32a8d30d92aa594a959b7ccd9e1c11
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-02-22 15:03:50 +0100

    Code Refactoring

--- recipe-app/src/app/app.component.ts
+++ recipe-app/src/app/app.component.ts
@@ -5,14 +5,4 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  isRecipeSelected: boolean = true;
-  isshoppingListSelected: boolean;
-
-  recipeSelected(event: boolean) {
-    this.isRecipeSelected = event;
-  }
-  shoppingListSelected(event: boolean) {
-    this.isshoppingListSelected = event;
-  }
-}
+export class AppComponent {}
