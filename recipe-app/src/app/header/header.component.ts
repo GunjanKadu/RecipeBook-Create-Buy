2361e0682a3abc71e8be67b55d36e9a043536a8b
Gunjan Kadu <gunjankadu@Gunjans-MacBook-Air.local>
2020-02-22 13:08:40 +0100

    adding Navgation using router links

--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,27 +1,9 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
 export class HeaderComponent {
-  recipeSelected: boolean;
-  @Output() isrecipeSelected = new EventEmitter<boolean>();
-
-  shoppingListSelected: boolean;
-  @Output() isshoppingListSelected = new EventEmitter<boolean>();
-
   constructor() {}
-  onRecipeClick() {
-    this.recipeSelected = true;
-    this.shoppingListSelected = false;
-    this.isrecipeSelected.emit(this.recipeSelected);
-    this.isshoppingListSelected.emit(this.shoppingListSelected);
-  }
-  onShoppingClick() {
-    this.recipeSelected = false;
-    this.shoppingListSelected = true;
-    this.isshoppingListSelected.emit(this.shoppingListSelected);
-    this.isrecipeSelected.emit(this.recipeSelected);
-  }
 }
