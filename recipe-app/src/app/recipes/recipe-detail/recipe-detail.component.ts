d865a08733be76a849679a8e677a15e94583c505
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-27 11:04:41 +0100

    Adding delete and cancel fucntionaltity

--- recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -24,6 +24,9 @@ export class RecipeDetailComponent implements OnInit {
   onEditRecipe() {
     this.router.navigate(["edit"], { relativeTo: this.route });
   }
+  onDeleteRecipe() {
+    this.recipeService.deleteRecipe(this.id);
+  }
   ngOnInit() {
     console.log(this.route);
     this.route.params.subscribe((params: Params) => {
