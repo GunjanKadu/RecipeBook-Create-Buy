dac51660360e33768230baaebb8371503ba51cff
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-29 15:17:47 +0200

    Creating and storing the user data

--- recipe-app/src/app/auth/auth.service.ts
+++ recipe-app/src/app/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { API_KEY } from "../Firebase";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
   kind: string;
@@ -17,6 +18,8 @@ export interface AuthResponseData {
 @Injectable({ providedIn: "root" })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  user = new Subject<User>();
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
@@ -28,7 +31,17 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
   login(email: string, password: string) {
     return this.http
@@ -41,7 +54,27 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
   private handleError(errorRes: HttpErrorResponse) {
     let errorMessage = "An Unknown Error Occured";
