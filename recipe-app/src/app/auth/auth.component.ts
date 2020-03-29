402dbe97f1794a62970d8d1c6b24d051cc4d3c9d
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-29 11:50:16 +0200

    Handling form input

--- recipe-app/src/app/auth/auth.component.ts
+++ recipe-app/src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-auth",
@@ -11,6 +12,10 @@ export class AuthComponent implements OnInit {
   constructor() {}
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
   ngOnInit() {}
 }
