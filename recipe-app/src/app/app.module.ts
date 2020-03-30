e4bbad27f7910c6fa613320c3c90565986329129
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-03-30 19:01:57 +0200

    Feature Modules and splitting modules correctly

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -5,36 +5,27 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
 import { AppDropDownDirective } from "./shared/app-drop-down.directive";
 import { ShoppingListService } from "./shopping-list/shoppinglist.service";
 import { AppRoutingModule } from "./app-routing.module";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { AuthComponent } from "../app/auth/auth.component";
 import { RecipeService } from "./recipes/recipe.service";
 import { LoadingSpinnerComponent } from "./shared/Loading Spinner/loadingSpinner.components";
 import { AuthInterceptorService } from "./auth/auth.interceptor.service";
 import { AlertComponent } from "./shared/ALert/alert.component";
+import { RecipesModule } from "./recipes/recipe.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
+
     ShoppingListComponent,
     ShoppingEditComponent,
     AppDropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent
@@ -44,7 +35,8 @@ import { AlertComponent } from "./shared/ALert/alert.component";
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModule
   ],
   providers: [
     ShoppingListService,
