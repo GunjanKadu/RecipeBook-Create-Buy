a2d748bc41056cdc8278579cb782e68315fc7edd
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-13 17:16:21 +0200

    Setting up actions and ngrx store

---
+++ recipe-app/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,16 @@
+import { Ingredient } from "../../shared/ingredients.model";
+import * as ShoppingListActions from "./shoppinglist.action";
+
+const initialState = {
+  ingredients: [new Ingredient("Apple", 5), new Ingredient("Tomatoes", 5)],
+};
+
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredients
+) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return { ...state, ingredients: [...state.ingredients, action.payload] };
+  }
+}
