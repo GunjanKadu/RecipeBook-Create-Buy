d1d4d71eb38ec0aa269836597762aa7efc856445
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-13 17:00:16 +0200

    Adding logic to the reducer

--- recipe-app/src/app/shopping-list/shopping-list.reducer.ts
+++ recipe-app/src/app/shopping-list/shopping-list.reducer.ts
@@ -1,7 +1,12 @@
 import { Ingredient } from "../shared/ingredients.model";
-
+import { Action } from "@ngrx/store";
 const initialState = {
   ingredients: [new Ingredient("Apple", 5), new Ingredient("Tomatoes", 5)],
 };
 
-export function shoppingListReducer(state = initialState, action) {}
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case "ADD_INGREDIENTS":
+      return { ...state, ingredients: [...state.ingredients, action] };
+  }
+}
