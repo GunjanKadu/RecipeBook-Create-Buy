ad7fc3e075642d9ea60e5a5b961dc3ff17d13817
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-17 19:18:21 +0200

    Handling synchronous auth from redux

--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -2,23 +2,30 @@ import { Component, OnInit, OnDestroy } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage-service";
 import { AuthService } from "../auth/auth.service";
 import { Subscription } from "rxjs";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: "app-header",
-  templateUrl: "./header.component.html"
+  templateUrl: "./header.component.html",
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   private susbscription: Subscription;
   isAuthenticated: boolean = false;
   constructor(
     private dataStorageService: DataStorageService,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
   ) {}
 
   ngOnInit() {
-    this.susbscription = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !user ? false : true;
-    });
+    this.susbscription = this.store
+      .select("auth")
+      .pipe(map((authState) => authState.user))
+      .subscribe((user) => {
+        this.isAuthenticated = !user ? false : true;
+      });
   }
   ngOnDestroy() {
     this.susbscription.unsubscribe();
