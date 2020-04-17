9956fa7884107000f41a4920486f715f5707fc73
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-17 18:39:08 +0200

    Setting up auth reducers and actions

--- recipe-app/src/app/auth/store/auth.reducer.ts
+++ recipe-app/src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user.model";
+import * as AuthAction from "./auth.action";
 
 export interface State {
   user: User;
@@ -7,6 +8,22 @@ export interface State {
 const initialState: State = {
   user: null,
 };
-export function authReducer(state = initialState, action) {
-  return state;
+export function authReducer(
+  state = initialState,
+  action: AuthAction.authAction
+) {
+  switch (action.type) {
+    case AuthAction.LOGIN:
+      const user = new User(
+        action.payload.email,
+        action.payload.userId,
+        action.payload.token,
+        action.payload.expirationDate
+      );
+      return { ...state, user: user };
+    case AuthAction.LOGOUT:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
 }
